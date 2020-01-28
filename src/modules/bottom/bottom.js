05f02902febd00f1b612de4d6b47bb406c1f6152
shinshilkina <anastasiashilina98@gmail.com>
2020-01-28 15:16:19 +0700

    make bottom elements

---
+++ src/modules/bottom/bottom.js
@@ -0,0 +1,11 @@
+'use strict';
+import './bottom-data.pug';
+import './bottom.scss';
+import $ from 'jquery';
+
+$('div.-navigation').addClass("--height");
+$('div.-about_us').addClass("--height");
+$('div.-support').addClass("--height");
+
+
+

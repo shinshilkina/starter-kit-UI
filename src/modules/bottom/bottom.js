c036e1d4752c1f6f48c6419ee9ced9469f7678e1
shinshilkina <anastasiashilina98@gmail.com>
2020-02-01 18:27:41 +0700

    reduct code

--- src/modules/bottom/bottom.js
+++ src/modules/bottom/bottom.js
@@ -7,5 +7,13 @@ $('div.-navigation').addClass("--height");
 $('div.-about_us').addClass("--height");
 $('div.-support').addClass("--height");
 
-
-
+function foo() {
+    let windowWidth = document.documentElement.clientWidth;
+    if (windowWidth < 980) {
+        const logoLinks = document.querySelector('.bottom__down__links').style;
+        const bottomBlock = document.querySelector('.bottom__down').style;
+        logoLinks.paddingTop = 90 + "px";
+        bottomBlock.height= 160 + "px";
+    }
+}
+foo();

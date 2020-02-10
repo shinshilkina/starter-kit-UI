a5cb78d08b74959abd7586473ce6281b568dc7e6
shinshilkina <anastasiashilina98@gmail.com>
2020-02-10 21:42:04 +0700

    try make dropdown

--- src/modules/search_room/search_room.js
+++ src/modules/search_room/search_room.js
@@ -1,1 +1,12 @@
 import './search_room.scss'
+import $ from 'jquery';
+require('jquery-mask-plugin');
+
+window.$ = window.jQuery = require('jquery');
+var mask = require('jquery-mask-plugin');
+$.mask = mask;
+window.mask = mask;
+
+$(document).ready(function($){
+    $('.date').mask("00.00.0000");
+});

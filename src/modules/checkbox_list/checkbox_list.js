ccd9b98b5d8b5d7f02e371f5b94c82b2213eff2d
shinshilkina <anastasiashilina98@gmail.com>
2020-03-25 22:47:41 +0700

    mini-changes of checkbox list

--- src/modules/checkbox_list/checkbox_list.js
+++ src/modules/checkbox_list/checkbox_list.js
@@ -1,10 +1,11 @@
 import './checkbox_list.scss';
 
-const checkBoxListButton = document.querySelectorAll('.checkbox-list .checkbox-list_select');
-for (let button of checkBoxListButton) {
-    button.addEventListener('click', function () {
-        const checkBoxItems = button.parentElement.nextElementSibling;
+const checkBoxListItem = document.querySelectorAll('.checkbox-list .checkbox-list_title');
+for (let item of checkBoxListItem) {
+    item.addEventListener('click', function () {
+        const checkBoxItems = item.parentElement.querySelector('.checkbox-list_items');
         checkBoxItems.classList.toggle('invisible');
+        const button = item.querySelector('.checkbox-list_select');
         button.classList.toggle('rotate');
     });
 }

cf31c247ee1f5e57e99d79112038540d27979e53
shinshilkina <anastasiashilina98@gmail.com>
2020-04-17 18:09:44 +0700

    change properties on mixins, styles and add modules to webpack config

--- src/elements/masked-text-field/masked-text-field.js
+++ src/elements/masked-text-field/masked-text-field.js
@@ -4,11 +4,15 @@ import Inputmask from 'inputmask';
 const maskedInputs = document.querySelectorAll('.masked-text-field_input');
 
 for (let input of maskedInputs) {
-    Inputmask('datetime', {
-        inputFormat: "dd.mm.yyyy",
-        showMaskOnHover:false,
-        showMaskOnFocus: false
-    }).mask(input);
+    if (input.classList.contains('email')) {
+        Inputmask("email").mask(input);
+    } else if (!input.classList.contains('password')) {
+        Inputmask('datetime', {
+            inputFormat: "dd.mm.yyyy",
+            showMaskOnHover:false,
+            showMaskOnFocus: false
+        }).mask(input);
+    }
 }
 
 
--- src/entry.js
+++ src/entry.js
@@ -2,5 +2,6 @@ function requireAll(requireContext) {
     return requireContext.keys().map(requireContext);
 }
 
-requireAll(require.context('./modules', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
+requireAll(require.context('./elements', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
 requireAll(require.context('./pages', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
+requireAll(require.context('./modules', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));

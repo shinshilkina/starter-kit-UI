a7f5ab3702cc2be811e5dd84b760ac4c68d96e06
shinshilkina <anastasiashilina98@gmail.com>
2020-04-25 19:03:43 +0700

    make footer&bottom and change some style of elements

--- src/elements/header/header.js
+++ src/elements/header/header.js
@@ -6,18 +6,22 @@ import $ from 'jquery';
 function checkHeaderWidth() {
     let windowWidth = document.documentElement.clientWidth;
     const buttonRedister = document.querySelector('.register-header').style;
-    const username = document.querySelector('.navigation__element--name').style;
+    const username = document.querySelector('.navigation__element--name');
 
     if (windowWidth < 550) {
         buttonRedister.marginLeft = "auto";
         buttonRedister.marginRight = "auto";
-        username.marginLeft = "auto";
-        username.marginRight = "auto";
+        if (username) {
+            username.style.marginLeft = "auto";
+            username.style.marginRight = "auto";
+        }
     }
     else {
         buttonRedister.margin = 'auto 8.75rem auto 0';
-        username.marginLeft = "3rem";
-        username.marginRight = "8.8rem";
+        if (username) {
+            username.style.marginLeft = "2.9rem";
+            username.style.marginRight = "8.8rem";
+        }
     }
 }
 
